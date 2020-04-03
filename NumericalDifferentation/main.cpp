5860a36f1c6c523347e9660153b4d3ca012a48c6
Charles Summers <pl.charlessummers@gmail.com>
2020-04-03 14:42:05 +0200

    Second order derivative

--- NumericalDifferentation/main.cpp
+++ NumericalDifferentation/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 double f(double x)										// Hard-coded function for numerical analysis.
 {
-	return x * x*x - 2 * x*x + 3 * x - 6;
+	return x * x * x - 2 * x * x + 3 * x - 6;
 }
 
 double forwardDifferenceAt(double x, double h, int n)		// D^n[f(x)] = D[D^(n-1)[f(x+h)] - D^(n-1)[f(x)]]
@@ -16,52 +16,38 @@ double forwardDifferenceAt(double x, double h, int n)		// D^n[f(x)] = D[D^(n-1)[
 	return forwardDifferenceAt(x + h, h, n - 1) - forwardDifferenceAt(x, h, n - 1);
 }
 
-double firstDerivativeValueAt(double x, double h, int n)
-{
-	double finalValue = 0;
-	double formulaPart = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		formulaPart = forwardDifferenceAt(x, h, i);		// i-th forward difference at point x, with step h (up to n-th difference)
-		if (i % 2)
-			formulaPart /= i;							// f, 1/3 f, 1/5 f, 1/7 f, etc
-		else
-			formulaPart /= -i;							// -1/2 f, -1/4 f, -1/6 f, etc
-		finalValue += formulaPart;
-	}
-	return finalValue / h;
-}
-
 double * getSecondOrderCoefficients(double * initialCoefficients, int n)
 {
 	double * secondCoefficients = new double[n + 1];
 	for (int i = 0; i <= n; i++)
 		secondCoefficients[i] = 0;
-	for (int i = 1; i < (n + 1) / 2; i++)
-		for (int j = 1; j < (n + 1) / 2; j++)
-			secondCoefficients[i + j] += initialCoefficients[i] * initialCoefficients[j];
-	//for (int i = 0; i <= n; i++)
-	//	cout << "\n/\\" << i << ": " << secondCoefficients[i];
-	//system("pause");
+	for (int i = 1; i < n; i++)
+		for (int j = 1; j < n; j++)
+			if (i + j <= n)						// we don't care about the coefficients beyond n
+				secondCoefficients[i + j] += initialCoefficients[i] * initialCoefficients[j];
+			else
+				continue;
+	for (int i = 0; i <= n; i++)
 	return secondCoefficients;
-}
-
-double secondDerivativeValueAt(double x, double h, int n)
-{
-	double finalValue = 0;
-	double formulaPart = 0;
-	double * initialCoefficients = new double[(n + 1) / 2];		// size = ceil[n/2]
-	for (int i = 1; i <= n / 2; i++)	// for the sake of easy computation we treat i=0 as 'zero difference' which is not used
-		initialCoefficients[i] = 1 / i * pow(-1, (i + 1) % 2);
-	auto secondCoefficients = getSecondOrderCoefficients(initialCoefficients, n);
-	for (int i = 2; i <= n; i++)
-		finalValue += forwardDifferenceAt(x, h, i) * secondCoefficients[i];
-	return finalValue / (h * h);
 }
 
 double derivativeValueAt(double x, double h, int n, int k)
 {
-	return k == 1 ? firstDerivativeValueAt(x, h, n) : secondDerivativeValueAt(x, h, n);
+	double finalValue = 0;
+	double * initialCoefficients = new double[n + 1];
+	initialCoefficients[0] = 0;
+	for (int i = 1; i <= n; i++)
+		initialCoefficients[i] = pow(-1, (i + 1) % 2) / i;		// oscilating harmonic series
+
+	double * coefficients;
+	if (k == 1)
+		coefficients = initialCoefficients;
+	else
+		coefficients = getSecondOrderCoefficients(initialCoefficients, n);
+
+	for (int i = 2; i <= n; i++)
+		finalValue += forwardDifferenceAt(x, h, i) * coefficients[i];
+	return k == 1 ? finalValue / h : finalValue / (h * h);
 }
 
 int factorial(int n)
@@ -89,6 +75,7 @@ void solveForAccuracy(double x, double h, int n)
 {
 	double accuracy;
 	double newH = h;											// New Age (pun intended)
+	double newN = n;
 	cout << "Enter wanted approximation accuracy: ";
 	cin >> accuracy;
 	do
@@ -98,7 +85,13 @@ void solveForAccuracy(double x, double h, int n)
 		if (accuracy > errorAt(x, newH, n))
 			newH *= 1.05;
 	} while (abs(accuracy - abs(errorAt(x, newH, n))) > 0.0000001);
+	do
+	{
+		if (accuracy < errorAt(x, h, newN))
+			newN++;
+	} while (abs(accuracy - abs(errorAt(x, h, newN))) > 0.01);
 	cout << "The step value h for accuracy of " << accuracy << " is " << newH << endl;
+	cout << "The diff. order n for accuracy of " << accuracy << " is " << newN << endl;
 	system("pause");
 }
 
@@ -117,8 +110,11 @@ int main()
 		cout << "Enter xi - the point at which the value of the derivative should be determined: " << xi << endl << endl;
 		cout << "Derivative (" << k << ") value at " << xi << " of x^3 - 2x^2 + 3x - 6: ";
 		cout << derivativeValueAt(xi, h, n, k) << endl;
-		cout << "Error at " << xi << ": " << errorAt(xi, h, n) << endl << endl;
-		cout << "Require accuracy\t[A]\nReinitialize values\t[R]" << endl;
+		if (k == 1)
+			cout << "Error at " << xi << ": " << errorAt(xi, h, n) << endl << endl << "Require accuracy\t[A]";
+		else
+			cout << endl << endl;		// error formulae not found for 2nd derivative
+		cout << "\nReinitialize values\t[R]" << endl;
 		cout << "Change derivative order\t[1 / 2]\nTweak (n)\t\t[B / M]\nTweak (h)\t\t[G / J]\nTweak (xi)\t\t[Z / C]" << endl;
 		cout << "Quit\t\t\t[Esc]" << endl << endl;
 
